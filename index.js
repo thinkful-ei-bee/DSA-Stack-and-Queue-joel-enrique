e0b8378b1c67f2f13f6ac1063da060db1ed7f51b
Joel Corey <joelcorey@fastmail.com>
2019-05-17 09:31:05 -0700

    wip - isValid

--- index.js
+++ index.js
@@ -28,6 +28,39 @@ function is_palindrome(s) {
   return false;
 }
 
+function isEmpty(stack) {
+  if(stack.top === null) {
+    return true;
+  }
+  return false;
+}
+
+function isValid(str, stack, open = 0) {
+  
+  console.log(str)
+  if ((str.length === 0) && (isEmpty)) {
+    return true;
+  }
+
+  // if (isEmpty(stack)) {
+  //   return true;
+  // }
+  // else {
+  //   return false;
+  // }
+  
+
+  const char = str.charAt(0);
+  if (char === "(") {
+    stack.push(char);
+  }
+  if (char === ")") {
+    stack.pop();
+
+  }
+  //return isValid(str.slice(1), stack, open);
+};
+
 function main() {
   let startrek = new stack;
 
@@ -37,7 +70,13 @@ function main() {
   startrek.push('Scotty')  
   //display(peek(startrek));
 
-  console.log(is_palindrome('A man, a plan, a canal: Panama'));
+  // console.log(is_palindrome("dad"));
+  // console.log(is_palindrome('A man, a plan, a canal: Panama'));
+  // console.log(is_palindrome("1001"));
+  // console.log(is_palindrome("Tauhida"));
+
+  let stackStr = new stack;
+  console.log(isValid('(((()))', stackStr));
 }
 
 main()
